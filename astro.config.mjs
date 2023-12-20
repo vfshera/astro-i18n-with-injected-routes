ff72b0150369bf256fb1f9327d04e791131537f6
Franklin Shera <fshera96@gmail.com>
2023-12-20 04:12:48 +0300

    tailwind styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import { i18nRoutes } from "./src/i18n/integration";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [i18nRoutes()],
+  integrations: [i18nRoutes(), tailwind()]
 });
