58ad931926e034195b762df52c62e146ff1bce9a
Franklin Shera <fshera96@gmail.com>
2023-12-22 17:40:02 +0300

    add hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [i18nRoutes(), tailwind()],
-  output: "server",
+  output: "hybrid",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
