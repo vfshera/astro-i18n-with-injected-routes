cfa73148edddc7cc9bd51a4ab66a25862ccfd045
Franklin Shera <fshera96@gmail.com>
2023-12-22 17:30:00 +0300

    add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import { i18nRoutes } from "./src/i18n/integration";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [i18nRoutes(), tailwind()]
+  integrations: [i18nRoutes(), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
